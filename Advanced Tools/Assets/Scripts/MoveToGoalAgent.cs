33db37454d1e6ad94821de090140a3301606a6f2
DariaCristiana24 <dariacristiana24@gmail.com>
2024-05-10 14:41:40 +0200

    created a simple move to goal agent
    - restarted the project on a basic level to see what went wrong

--- Advanced Tools/Assets/Scripts/MoveToGoalAgent.cs
+++ Advanced Tools/Assets/Scripts/MoveToGoalAgent.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
+using System;
 
 public class MoveToGoalAgent : Agent
 {
     [SerializeField]
-    float speed = 5;
+    float speed = 1;
     [SerializeField]
     Transform target;
     [SerializeField]
     Renderer floor;
-    [SerializeField]
-    List<Transform> legJoints = new List<Transform>();
+
 
     public override void OnEpisodeBegin()
     {
@@ -24,46 +24,21 @@ public class MoveToGoalAgent : Agent
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.localPosition);
-        sensor.AddObservation(target.position);
-        foreach (var joint in legJoints)
-        {
-            sensor.AddObservation(joint.rotation);
-        }
-
+        sensor.AddObservation(target.localPosition);
 
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        //float moveX = actions.ContinuousActions[0];
-        //float moveZ = actions.ContinuousActions[1];
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
 
-        //transform.position += new Vector3(moveX, 0, moveZ)* Time.deltaTime* speed;
+        //int act = actions.DiscreteActions[0];
 
-        float rotX = actions.ContinuousActions[0];
-        float rotZ = actions.ContinuousActions[1];
-        int leg = actions.DiscreteActions[0];
-
-        Quaternion rot = Quaternion.Euler(rotX*30 * speed, 0, rotZ*30*speed);
-
-        //legJoints[leg].rotation =  rot;
-        legJoints[leg].rotation = Quaternion.Lerp(legJoints[leg].rotation, rot, Time.time * speed);
-        //transform.position += new Vector3(moveX, 0, moveZ)* Time.deltaTime* speed;
+           transform.position += new Vector3(moveX, 0, moveZ)* Time.deltaTime* speed;
+       // transform.position += new Vector3(moveX, 0, 0) * Time.deltaTime * speed;
+        Debug.Log(moveX);
     }
 
-    private void Update()
-    {
-        if(transform.localPosition.x > 0)
-        {
-            SetReward(0.001f * transform.localPosition.x);
-        }
-        if (transform.position.y < -10)
-        {
-            SetReward(-10); 
-            floor.material.color = Color.red;
-            EndEpisode();
-        }
-
-    }
 
     private void OnTriggerEnter(Collider other)
     {
